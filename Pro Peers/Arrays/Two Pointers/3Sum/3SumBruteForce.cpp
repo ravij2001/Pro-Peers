36521a4a18a004c29422a97cddd3a65cb4f4d9e2
Ravij Parikh <Ravijparikh0@gmail.com>
2025-10-03 22:49:23 +0530

    3 sum brute force approach

---
+++ Pro Peers/Arrays/Two Pointers/3Sum/3SumBruteForce.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<set>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        set<vector<int>> st;
+        for(int i = 0; i < nums.size() ; i++){
+            for(int j = i + 1; j < nums.size(); j++){
+                for(int k = j + 1 ; k < nums.size() ; k++){
+                    if(nums[i] + nums[j] + nums[k] == 0){
+                        vector<int> temp = {nums[i],nums[j],nums[k]};
+                        sort(temp.begin(),temp.end());
+                        st.insert(temp);
+                    }
+                }
+            }
+        }
+        vector<vector<int>> ans(st.begin(),st.end());
+        return ans;
+    }
+};
