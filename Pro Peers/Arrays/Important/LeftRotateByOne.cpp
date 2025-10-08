da5244047591046acdc960086344591c56eddbc6
Ravij Parikh <Ravijparikh0@gmail.com>
2025-10-08 11:59:07 +0530

    left rotate array by one

---
+++ Pro Peers/Arrays/Important/LeftRotateByOne.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    void rotateArrayByOne(vector<int>& nums) {
+        int temp = nums[0];
+        for(int i = 0; i < nums.size() - 1; i++){
+            nums[i] = nums[i+1];
+        } 
+        nums[nums.size() - 1] = temp;
+    }
+};
